0f07174886d116ad7b75d33e73b6f476d34b7102
satoshi-ota <satoshi.ota928@gmail.com>
2023-04-20 09:49:51 +0900

    fix(avoidance): fill stop time
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

--- planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
+++ planning/behavior_path_planner/src/scene_module/avoidance/avoidance_module.cpp
@@ -117,15 +117,22 @@ bool AvoidanceModule::isExecutionRequested() const
   }
 #else
   fillShiftLine(avoid_data, debug_data_);
-
-  if (avoid_data.unapproved_new_sl.empty()) {
-    return false;
-  }
 #endif
 
   if (parameters_->publish_debug_marker) {
     setDebugData(avoid_data, path_shifter_, debug_data_);
   }
+
+#ifndef USE_OLD_ARCHITECTURE
+  // there is object that should be avoid. return true.
+  if (!!avoid_data.stop_target_object) {
+    return true;
+  }
+
+  if (avoid_data.unapproved_new_sl.empty()) {
+    return false;
+  }
+#endif
 
   return !avoid_data.target_objects.empty();
 }
